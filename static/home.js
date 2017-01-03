93080c3c548db39ba2be1d59387b06efd08c3f41
Joshua <joshua.f.day@gmail.com>
2017-01-02 16:15:27 -0800

    form handling with JS function. Needs processing and math

--- static/home.js
+++ static/home.js
@@ -1,23 +1,35 @@
 (function main(){
   'use strict';
+  /*
+  Table of contents:
+    1. Graph Creation --  create_charts()
+    2.
+    3. Dosage time/amount section -- cont()
+    4. Creating events needed on page load -- events()
+    5. Sizing of the users input menu so it stays reactive and does not change page flow -- menu_sizing()
+  */
 
-
-  function handleform(){
+  //////////////////////////////////////// 1 ///////////////////////////////////
+  function create_charts(){
     console.log('lmao')
   }
 
+  //////////////////////////////////////// 2 ///////////////////////////////////
 
+  //////////////////////////////////////// 3 ///////////////////////////////////
   function cont(dosing_var){
     $('#time').timepicker();
-    var dose_time_field = '</br><input type="number" name="dose" min="1" placeholder="Dose" required>mg</br><input class="time" type="text" name="time" placeholder="Time" required>'
+    var dose_time_field = '</br><input type="number" name="dose" min="1" placeholder="Dose" class="dose" required>mg</br><input class="time" type="text" name="time" placeholder="Time" required>'
     var $dose_div = $('#dosing-amounts')
-    var $splits = $('select[name=doses_daily]').val()
+    var $splits = $('select[name=doses-daily]').val()
 
     if(dosing_var[0] === 'no' & dosing_var[1] === 'yes'){
+      $('#dose_form').attr('name') == 'form-ny'
       $dose_div.html('</br><span>Days 1-'+ dosing_var[2] +'</span>');
       $dose_div.append(dose_time_field)
 
     } else if(dosing_var[0] === 'yes' & dosing_var[1] === 'yes'){
+      $('#dose_form').attr('name') == 'form-yy'
       $dose_div.html('</br><span>Days 1-' + dosing_var[2] +'</span></br>');
 
       while($splits != 0){
@@ -26,6 +38,7 @@
       };
 
     } else if(dosing_var[0] === 'no' & dosing_var[1] === 'no'){
+      $('#dose_form').attr('name') == 'form-nn'
       var day = 1;
 
       while(day <= dosing_var[2]){
@@ -36,10 +49,10 @@
 
     $('.time').timepicker();
     $('#cont-btn').remove();
-    $('#submit').append('<button type="submit" class="btn btn-primary">Submit</button>');
+    $('#submit').append('<button type="submit" class="btn btn-primary" id="submit">Submit</button>');
   };
 
-
+  //////////////////////////////////////// 4 ///////////////////////////////////
   (function events(){
     $('#cont-btn').on('click', function(evt){
       evt.preventDefault();
@@ -52,11 +65,11 @@
         cont(dosing_var);
       } else {
         console.log(dosing_var)
-        $('#warning').text('The Split dosing, Same dose & Time of Dose, and the Cycle Length sections must be filled out.')
+        alert('The Split dosing, Same dose & Time of Dose, and the Cycle Length sections must be filled out.')
       }
     });
   })();
-
+  //////////////////////////////////////// 5 ///////////////////////////////////
   (function menu_sizing(){
     $(document).ready(function(){
       $('#menu').css('height', window.innerHeight);
